eac39889036926b969e2fb123bc7ba33595bd07f
kobamiha <mihatu.irisawa@twinplanet.co.jp>
2023-01-10 14:30:08 +0900

    次世代画像対応

--- assets/uptfooter.js
+++ assets/uptfooter.js
@@ -48,3 +48,37 @@ function subscFaq(){
 }
 subscFaq();
 });
+
+
+
+async function supportsWebp() {
+    if (!self.createImageBitmap) return false;
+    
+    const webpData = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=';
+    const blob = await fetch(webpData).then(r => r.blob());
+    return createImageBitmap(blob).then(() => true, () => false);
+    
+  }
+  
+  (async () => {
+    if(await supportsWebp()) {
+      console.log('does support');
+    }
+    else {
+      console.log('does not support');
+    }
+  })();
+
+
+  const addWebpDetectionClass = () => {
+    if (supportsWebp()) {
+      document.body.classList.add('is-webp');
+
+    } else {
+        document.body.classList.add('is-no-webp');
+    }
+  };
+  
+  addWebpDetectionClass(); // 実行
+  
+
